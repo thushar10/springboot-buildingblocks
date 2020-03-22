eadac168d39c8d41a727056fffd9781f06ed9d70
LAPTOP-K34MNMIS\thushar p sujir <thushar.gokusujir@gmail.com>
2020-03-22 17:25:33 +0530

    Second commit

--- src/main/java/com/boot/project/basic/BasicApplication.java
+++ src/main/java/com/boot/project/basic/BasicApplication.java
@@ -2,7 +2,7 @@ package com.boot.project.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class BasicApplication {
 
